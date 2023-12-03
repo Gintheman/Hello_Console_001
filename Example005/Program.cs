6cb70624b305b0bfead1449c81e5b34fc581ec29
Eugene <joker3lolz@gmail.com>
2023-12-03 17:50:03 +0300

    Example005 + some code inside Program.cs

---
+++ Example005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter user name");
+string username = Console.ReadLine();
+if(username.ToLower() == "Masha")
+{
+    Console.WriteLine("MASHKA-KAKASHKA");
+}
+else 
+{
+    Console.WriteLine("Hello," + username);
+}
